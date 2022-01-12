7c89d7a40013dceab202f86a4f67d4cee433ca52
w0rstR <sat2151@gmail.com>
2022-01-12 21:09:10 +0200

    Finished work with spacex api

---
+++ react-practic-1/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FlightList from "./components/FlightList/FlightList";
+
+function App() {
+  return (
+    <div className="App">
+        <FlightList/>
+    </div>
+  );
+}
+
+export default App;
