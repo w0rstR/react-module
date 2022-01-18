77e6f258134ad4c679918aa4e58bfa0fac564a38
w0rstR <sat2151@gmail.com>
2022-01-18 12:19:00 +0200

    first com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import PostPage from "./pages/PostPage/PostPage";
 
 function App() {
   return (
-
     <div className="App">
+        <div className="header">
+          <a className={'item'} href="/home">Home</a>
+          <a className={'item'} href="/about">About</a>
+          <a className={'item'} href="/post">Post</a>
+        </div>
+
+        <Routes>
+            <Route path={'/home'} component={<HomePage/>}/>
+            <Route path={'/about'} component={<AboutPage/>}/>
+            <Route path={'/post'} component={<PostPage/>}/>
+        </Routes>
 
     </div>
   );
